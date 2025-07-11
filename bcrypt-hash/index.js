dcc1beef6cb1a861d9b5e0c0ff3a18d4d852483d
Baron Stubblefield <baron.stubblefield@knobbe.com>
2025-07-11 10:44:06 -0700

    Fix formatting by adding missing semicolons in index.js

--- bcrypt-hash/index.js
+++ bcrypt-hash/index.js
@@ -1,11 +1,11 @@
-const bcrypt = require("bcryptjs")
+const bcrypt = require("bcryptjs");
 
-const knobbe_apikey = "0f500c90-9df1-4bd4-b789-b89f1a1bcf65"
+const knobbe_apikey = "0f500c90-9df1-4bd4-b789-b89f1a1bcf65";
 const isotimestamp = new Date().toISOString();
 const authhash = knobbe_apikey + "|" + isotimestamp;
 
 const cryptfunction = async () => {
-    const hashedKey = await bcrypt.hash(authhash, 10)
+    const hashedKey = await bcrypt.hash(authhash, 10);
     console.log(knobbe_apikey);
     console.log(isotimestamp);
     console.log(hashedKey);
