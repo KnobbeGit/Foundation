6f60f8e66d98dcff60f12ca32ae52ae4e1168e14
jplaub <jp.laub@knobbe.com>
2025-07-11 09:35:33 -0700

    bcryptjs working in bcrypthas/index.js

--- bcrypt-hash/index.js
+++ bcrypt-hash/index.js
@@ -1,8 +1,15 @@
-import bcrypt from 'bcryptjs';
+const bcrypt = require("bcryptjs")
 
-const authhash = {knobbe_apikey}|{isotimestamp};
-console.log({
-    authhash,
-    hashed: bcrypt.hashSync(authhash, 10),
-    timestamp: new Date().toISOString()
-})
+const knobbe_apikey = "0f500c90-9df1-4bd4-b789-b89f1a1bcf65"
+const isotimestamp = new Date().toISOString();
+const authhash = knobbe_apikey + "|" + isotimestamp;
+
+const cryptfunction = async () => {
+    const hashedKey = await bcrypt.hash(authhash, 10)
+    console.log(knobbe_apikey);
+    console.log(isotimestamp);
+    console.log(hashedKey);
+};
+
+cryptfunction()
+
